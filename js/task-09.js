ed56846865ee78790940fe2325299d19ea812e76
DanylenkoTetiana <danilenko.tanya@i.ua>
2022-09-29 23:03:47 +0300

    add 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+'user strict';
+const btnEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+
+const handleClick = callback => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+};
+
+btnEl.addEventListener('click', handleClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
