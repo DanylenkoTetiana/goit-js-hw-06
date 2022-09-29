e40fc80f564ebd702add19c939bc4b2e70be3e30
DanylenkoTetiana <danilenko.tanya@i.ua>
2022-09-29 23:20:57 +0300

    add 7

--- js/task-07.js
+++ js/task-07.js
@@ -2,12 +2,7 @@
 const inputEl = document.querySelector('#font-size-control');
 const spanEl = document.querySelector('#text');
 
-console.log(spanEl);
-
-inputEl.addEventListener('input', event => {
-  console.log(Number(event.currentTarget.value));
-  spanEl.style.fontSize = '16px';
-  const nkjknk = Number(event.currentTarget.value);
-  spanEl.style.fontSize = spanEl.style.fontSize + nkjknk;
-  console.log(spanEl.style.fontSize);
-});
+inputEl.addEventListener(
+  'input',
+  event => (spanEl.style.fontSize = `${Number(event.currentTarget.value) - 20}px`)
+);
